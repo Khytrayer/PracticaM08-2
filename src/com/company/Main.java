a01499354aae6e945dd228a8ac21e28f58721951
Khytrayer <manoleeric@gmail.com>
2023-06-05 18:28:57 +0200

    funcion media

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double[] numeros = {1,2,3,4,5};
+
+         media(numeros);
+
+
+        }
+
+        public static double media(double[] nums) {
+            double sol=0;
+            for (int i=0; i<nums.length;i++) {
+                sol+=nums[i];
+            }
+            sol=sol/nums.length;
+            System.out.println(sol);
+            return sol;
+        }
     }
-}
+
